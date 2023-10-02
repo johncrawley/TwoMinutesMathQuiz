bb92d90c8f32adbac55cfe837ebb082f468c2c2d
john <johncrawley1@gmail.com>
2023-10-02 11:12:03 +0100

    Added a bound service for the game.

--- app/src/main/java/com/jcrawley/tmmq/MainActivity.java
+++ app/src/main/java/com/jcrawley/tmmq/MainActivity.java
@@ -1,16 +1,70 @@
 package com.jcrawley.tmmq;
 
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView questionTextView;
+    private boolean bound = false;
+    private GameService gameService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        questionTextView = findViewById(R.id.questionText);
+     }
+
+
+    public void setQuestionText(String questionText){
+       questionTextView.setText(questionText);
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService.
+        Intent intent = new Intent(this, GameService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(connection);
+        bound = false;
+    }
+
+
+    private final ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance.
+            GameService.LocalBinder binder = (GameService.LocalBinder) service;
+            gameService = binder.getService();
+            bound = true;
+        }
+
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            bound = false;
+        }
+    };
+
 
 }
