dd03a02bc590c62809acf47d231fd7cb8274f623
john <johncrawley1@gmail.com>
2023-11-25 23:57:44 +0000

    Improving math operation limits corresponding to difficulty levels.

--- app/src/main/java/com/jcrawley/tmmq/service/game/level/LevelFactory.java
+++ app/src/main/java/com/jcrawley/tmmq/service/game/level/LevelFactory.java
@@ -11,48 +11,73 @@ public class LevelFactory {
 
         Map<Integer, GameLevel> levels = new HashMap<>(10);
         GameLevel gameLevel1 = new GameLevel();
-        addOperationLimitsTo(gameLevel1, MathOperation.ADDITION, 1,5,1,5);
+        addTo(gameLevel1, MathOperation.ADDITION, 1,5,1,5);
         levels.put(1, gameLevel1);
 
         GameLevel gameLevel2 = new GameLevel();
-        addOperationLimitsTo(gameLevel2, MathOperation.ADDITION, 1,10,1,10);
+        addTo(gameLevel2, MathOperation.ADDITION, 2,12,3,10);
         levels.put(2, gameLevel2);
 
-        GameLevel gameLevel3 = new GameLevel();
-        addOperationLimitsTo(gameLevel3, MathOperation.ADDITION, 3,12,2,12);
-        addOperationLimitsTo(gameLevel3, MathOperation.SUBTRACTION, 1,10,1,10);
-        levels.put(3, gameLevel3);
+        GameLevel level3 = new GameLevel();
+        addTo(level3, MathOperation.ADDITION, 4,15,3,15);
+        addTo(level3, MathOperation.SUBTRACTION, 1,12,4,15);
+        levels.put(3, level3);
 
-        GameLevel gameLevel4 = new GameLevel();
-        addOperationLimitsTo(gameLevel4, MathOperation.ADDITION, 4,15,4,15);
-        addOperationLimitsTo(gameLevel4, MathOperation.SUBTRACTION, 1,12,1,12);
-        levels.put(4, gameLevel4);
+        GameLevel level4 = new GameLevel();
+        addTo(level4, MathOperation.ADDITION, 5,20,5,20);
+        addTo(level4, MathOperation.SUBTRACTION, 3,15,4,20);
+        addTo(level4, MathOperation.MULTIPLICATION, 2,10,2,5);
+        levels.put(4, level4);
 
-        GameLevel gameLevel5 = new GameLevel();
-        addOperationLimitsTo(gameLevel5, MathOperation.ADDITION, 5,20,5,20);
-        addOperationLimitsTo(gameLevel5, MathOperation.SUBTRACTION, 1,12,1,12);
-        addOperationLimitsTo(gameLevel5, MathOperation.MULTIPLICATION, 2,5,2,5);
-        levels.put(5, gameLevel5);
+        GameLevel level5 = new GameLevel();
+        addTo(level5, MathOperation.ADDITION, 7,25,8,30);
+        addTo(level5, MathOperation.SUBTRACTION, 7,30,10,35);
+        addTo(level5, MathOperation.MULTIPLICATION, 3,12,5,12);
+        addTo(level5, MathOperation.DIVISION, 2,12,5,12);
+        levels.put(5, level5);
 
-        GameLevel gameLevel6 = new GameLevel();
-        addOperationLimitsTo(gameLevel6, MathOperation.ADDITION, 5,20,5,20);
-        addOperationLimitsTo(gameLevel6, MathOperation.SUBTRACTION, 1,12,1,12);
-        addOperationLimitsTo(gameLevel6, MathOperation.MULTIPLICATION, 2,10,2,10);
-        addOperationLimitsTo(gameLevel6, MathOperation.DIVISION, 2,10,2,10);
-        levels.put(6, gameLevel6);
+        GameLevel level6 = new GameLevel();
+        addTo(level6, MathOperation.ADDITION, 8,40,10,50);
+        addTo(level6, MathOperation.SUBTRACTION, 10,50,40,70);
+        addTo(level6, MathOperation.MULTIPLICATION, 2,12,12,15);
+        addTo(level6, MathOperation.DIVISION, 2,15,7,15);
+        levels.put(6, level6);
 
-        GameLevel gameLevel7 = new GameLevel();
-        addOperationLimitsTo(gameLevel7, MathOperation.ADDITION, 5,25,5,25);
-        addOperationLimitsTo(gameLevel7, MathOperation.SUBTRACTION, 5,15,8,15);
-        addOperationLimitsTo(gameLevel7, MathOperation.MULTIPLICATION, 2,12,2,12);
-        addOperationLimitsTo(gameLevel7, MathOperation.DIVISION, 4,12,2,12);
-        levels.put(7, gameLevel7);
+        GameLevel level7 = new GameLevel();
+        addTo(level7, MathOperation.ADDITION, 100,150,80,170);
+        addTo(level7, MathOperation.SUBTRACTION, 100,150,80,170);
+        addTo(level7, MathOperation.MULTIPLICATION, 5,15,12,20);
+        addTo(level7, MathOperation.DIVISION, 5,12,10,20);
+        levels.put(7, level7);
+
+        GameLevel level8 = new GameLevel();
+        addTo(level8, MathOperation.ADDITION, 150,250,125,300);
+        addTo(level8, MathOperation.SUBTRACTION, 120,170,130,300);
+        addTo(level8, MathOperation.MULTIPLICATION, 2,12,2,12);
+        addTo(level8, MathOperation.DIVISION, 8,15,2,40);
+        levels.put(8, level8);
+
+        GameLevel level9 = new GameLevel();
+        addTo(level9, MathOperation.ADDITION, 500,1000,700,2500);
+        addTo(level9, MathOperation.SUBTRACTION, 500,1500,800,2000);
+        addTo(level9, MathOperation.MULTIPLICATION, 15,30,15,40);
+        addTo(level9, MathOperation.DIVISION, 12,20,2,70);
+        levels.put(9, level9);
+
+
+        GameLevel level10 = new GameLevel();
+        addTo(level10, MathOperation.ADDITION, 10_000,20_000,12_111,25_123);
+        addTo(level10, MathOperation.SUBTRACTION, 10_000,15_000,20_000,50_000);
+        addTo(level10, MathOperation.MULTIPLICATION, 25,50,30,100);
+        addTo(level10, MathOperation.DIVISION, 25,50,25,99);
+        levels.put(10, level10);
+
 
         return levels;
     }
 
-    private static void addOperationLimitsTo(GameLevel gameLevel, MathOperation mathOperation, int number1Min,
-                                      int number1Max, int number2Min, int number2Max){
+    private static void addTo(GameLevel gameLevel, MathOperation mathOperation, int number1Min,
+                              int number1Max, int number2Min, int number2Max){
         gameLevel.addOperationLimits(mathOperation, new OperationLimits(number1Min, number1Max, number2Min, number2Max));
     }
 
--- app/src/main/java/com/jcrawley/tmmq/service/game/level/OperationLimits.java
+++ app/src/main/java/com/jcrawley/tmmq/service/game/level/OperationLimits.java
@@ -1,17 +1,15 @@
 package com.jcrawley.tmmq.service.game.level;
 
-import com.jcrawley.tmmq.service.game.question.MathOperation;
 
 public class OperationLimits {
 
-    private final boolean isEnabled;
+
     private final int firstNumberMin;
     private final int firstNumberMax;
     private final int secondNumberMin;
     private final int secondNumberMax;
 
     public OperationLimits(int firstNumberMin, int firstNumberMax, int secondNumberMin, int secondNumberMax){
-        this.isEnabled = true;
         this.firstNumberMin = firstNumberMin;
         this.firstNumberMax = firstNumberMax;
         this.secondNumberMin = secondNumberMin;
@@ -20,17 +18,12 @@ public class OperationLimits {
 
 
     public OperationLimits(){
-        this.isEnabled = false;
         firstNumberMin = 0;
         firstNumberMax = 0;
         secondNumberMin = 0;
         secondNumberMax = 0;
     }
 
-
-    public boolean isEnabled() {
-        return isEnabled;
-    }
 
     public int getFirstNumberMin() {
         return firstNumberMin;
